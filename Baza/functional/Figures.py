dbbea553ead84d57402f079cab29405fa2ec708b
Mafanua <maximka.sibiryakov@gmail.com>
2022-11-02 17:29:18 +0300

    comment for figures

--- Baza/functional/Figures.py
+++ Baza/functional/Figures.py
@@ -11,12 +11,14 @@ def cube(a):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
+    #Постороение куба через матрицу булевых элементов
     dots = [a,a,a]
     data = np.ones(dots, dtype=np.bool)
     indent = 0.9
     string = np.empty(dots + [4], dtype=np.float32)
     string[:] = [0, 0, 1, indent]
 
+    #Задание параметров построения
     ax.voxels(data, facecolors=string, )
 
     plt.savefig(img, format='png')
@@ -31,12 +33,14 @@ def ball(a):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
-    u = np.linspace(0, 2 * np.pi, 100)
+    #Постороение шара через тригонометрические функции(строит полигонами)
+    u = np.linspace(0, 2 * np.pi, 100) 
     v = np.linspace(0, np.pi, 100)
     x = a * np.outer(np.cos(u), np.sin(v))
     y = a * np.outer(np.sin(u), np.sin(v))
     z = a * np.outer(np.ones(np.size(u)), np.cos(v))
 
+    #Задание параметров построения
     ax.plot_surface(x, y, z, rstride=5, cstride=5, color='b')
 
     plt.savefig(img, format='png')
@@ -76,12 +80,14 @@ def cylinder(a, b):
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
 
+    #Постороение цилиндра через тригонометрические функции(строит полигонами)
     u = np.linspace (0,2 * np.pi, 100)
     h = np.linspace (0,a,20)
     x = b * np.outer (np.sin (u), np.ones (len (h)))
     y = b * np.outer (np.cos (u), np.ones (len (h)))
     z = np.outer (np.ones (len (u)), h)
 
+    #Задание параметров построения
     ax.plot_surface(x, y, z, color='b')
     plt.savefig(img, format='png')
     plt.close()
